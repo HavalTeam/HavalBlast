98b488921e2e3e924d1d9d90fab2cad62b442d95
Robert Malikov <Robert.Malikov@urfu.me>
2025-06-19 22:35:51 +0500

    пока так

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,13 +1,13 @@
 document.addEventListener('DOMContentLoaded', () => {
     const startGameBtn = document.querySelector('.start-game-btn');
-    const statBtn = document.querySelector('.stat-btn');
+    const recordsBtn = document.querySelector('.records-btn');
+    const recordsPopup = document.getElementById('records-popup');
     const helpBtn = document.querySelector('.help-btn');
     const helpPopup = document.getElementById('help-popup');
-    const recordsPopup = document.getElementById('records-popup');
     const closePopupBtns = document.querySelectorAll('.close-popup');
     const highScoreElement = document.getElementById('high-score');
 
-    // Загружаем лучший счет из localStorage
+
     const highScore = localStorage.getItem('highScore') || 0;
     highScoreElement.textContent = highScore;
 
@@ -15,7 +15,7 @@ document.addEventListener('DOMContentLoaded', () => {
         window.location.href = 'gamePage.html';
     });
 
-    statBtn.addEventListener('click', () => {
+    recordsBtn.addEventListener('click', () => {
         recordsPopup.style.display = 'block';
     });
 
@@ -23,20 +23,11 @@ document.addEventListener('DOMContentLoaded', () => {
         helpPopup.style.display = 'block';
     });
 
-    // Закрытие pop-up окон
     closePopupBtns.forEach(btn => {
         btn.addEventListener('click', () => {
             helpPopup.style.display = 'none';
             recordsPopup.style.display = 'none';
         });
     });
-
-    window.addEventListener('click', (event) => {
-        if (event.target === helpPopup) {
-            helpPopup.style.display = 'none';
-        }
-        if (event.target === recordsPopup) {
-            recordsPopup.style.display = 'none';
-        }
-    });
+    
 });
