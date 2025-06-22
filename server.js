7111bf9ffe04fafc15df991ff9743bf7db1d4d2d
dabbl <dabbler503@gmail.com>
2025-06-22 21:06:51 +0500

    node.js

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, 'src')));
+console.log(path.join(__dirname, 'src'));
+
+app.listen(PORT, () => {
+    console.log(`Сервер запущен: http://localhost:${PORT}`);
+});
