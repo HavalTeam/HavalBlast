e49bbd888988a6a3ff9dba759b0033cd7486732d
Robert Malikov <Robert.Malikov@urfu.me>
2025-06-23 09:28:40 +0500

    menu name form

--- src/js/menu.js
+++ src/js/menu.js
@@ -7,26 +7,66 @@ document.addEventListener('DOMContentLoaded', () => {
     const closePopupBtns = document.querySelectorAll('.close-popup');
     const highScoreElement = document.getElementById('high-score');
 
+    const nameDisplay = document.getElementById('current-user-name');
+    const nameForm = document.getElementById('user-name-form');
+    const nameInput = document.getElementById('user-name-input');
+
+    const savedName = localStorage.getItem('userName') || '';
+    let preventStart;
+    if (savedName) {
+        nameDisplay.textContent = savedName;
+        preventStart = false;
+    } else {
+        nameDisplay.textContent = '';
+        preventStart = true;
+    }
+
 
     const highScore = localStorage.getItem('highScore') || 0;
     highScoreElement.textContent = highScore;
 
+
+    nameForm.addEventListener('submit', e => {
+        e.preventDefault();
+        const name = nameInput.value.trim();
+        if (!name) {
+            nameInput.classList.add('error');
+            nameInput.addEventListener('animationend', () => {
+                nameInput.classList.remove('error');
+            }, { once: true });
+            return;
+        }
+
+        localStorage.setItem('userName', name);
+        nameDisplay.textContent = name;
+        preventStart = false;
+    });
+
+
     startGameBtn.addEventListener('click', () => {
+        if (preventStart) {
+            nameInput.classList.add('error');
+            nameInput.addEventListener('animationend', () => {
+                nameInput.classList.remove('error');
+            }, { once: true });
+            return;
+        }
+
         window.location.href = 'game.html';
     });
 
     recordsBtn.addEventListener('click', () => {
-        recordsPopup.style.display = 'block';
+        recordsPopup.classList.add('show');
     });
 
     helpBtn.addEventListener('click', () => {
-        helpPopup.style.display = 'block';
+        helpPopup.classList.add('show');
     });
 
     closePopupBtns.forEach(btn => {
         btn.addEventListener('click', () => {
-            helpPopup.style.display = 'none';
-            recordsPopup.style.display = 'none';
+            helpPopup.classList.remove('show');
+            recordsPopup.classList.remove('show');
         });
     });
     
